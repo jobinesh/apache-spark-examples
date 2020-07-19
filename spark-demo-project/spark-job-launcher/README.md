71fad9a38b30d893ca356e2f792a5a657272e2c1
Jobinesh Purushothaman <jobinesh.purushothaman@oracle.com>
2020-07-19 13:45:57 -0700

    Added spark launcher module

---
+++ spark-demo-project/spark-job-launcher/README.md
@@ -0,0 +1,5 @@
+## The SparkLauncher helps you to start Spark applications programmatically. 
+$ cd <spark-demo-project>      
+$ mvn clean install  
+$ cd <spark-job-launcher>    
+$ mvn exec:java -Dexec.mainClass="com.jobinesh.spark.job.SparkJobLauncher"    
---
+++ spark-demo-project/spark-job-launcher/src/main/scala/com/jobinesh/spark/job/SparkJobLauncher.scala
@@ -0,0 +1,29 @@
+package com.jobinesh.spark.job
+
+import org.apache.spark.launcher.SparkAppHandle
+
+object SparkJobLauncher {
+  def main(implicit args: Array[String]): Unit = {
+    import org.apache.spark.launcher.SparkLauncher
+    import java.util.concurrent.CountDownLatch
+    val countDownLatch = new CountDownLatch(1)
+    val spark = new SparkLauncher().setAppResource("/Users/jmpurush/mywork/my-learnings/apache-spark-examples/spark-demo-project/spark-job-impl/target/spark-job-impl-LATEST-SNAPSHOT.jar")
+      .setMainClass("com.jobinesh.spark.job.impl.WordCountJob")
+      .setMaster("local")
+      .addSparkArg("--packages", "com.typesafe:config:1.4.0,com.jobinesh.example:spark-job-common:LATEST-SNAPSHOT,com.jobinesh.example:spark-job-impl:LATEST-SNAPSHOT")
+      .startApplication(new SparkAppHandle.Listener() {
+        def infoChanged(handle: SparkAppHandle): Unit = {
+        }
+
+        def stateChanged(handle: SparkAppHandle): Unit = {
+          val appState = handle.getState()
+          println(s"Spark App Id [${handle.getAppId}] State Changed. State [${handle.getState}]")
+
+          if (appState != null && appState.isFinal) {
+            countDownLatch.countDown //waiting until spark driver exits
+          }
+        }
+      })
+    countDownLatch.await()
+  }
+}
